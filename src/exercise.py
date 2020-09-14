3ecd85650325197365152327e5310700884807ea
RhysJones12 <21314@bridgend.ac.uk>
2020-09-14 08:30:36 +0000

    1.4 final

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #write your code below this line
     message = input("Write a message...")
-  print "Once upon a time...".
+  print "Once upon a time"
 
 if __name__ == '__main__':
     main()
