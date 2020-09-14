fd4b9213c7299d909034f27e942896ef06f97fa0
RhysJones12 <21314@bridgend.ac.uk>
2020-09-14 08:41:37 +0000

    1.4 the right version

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,6 @@
 def main():
-    #write your code below this line
-    message = input("Write a message...")
-  print "Once upon a time..."
+    message = input("bye")
+    print (message)
 
 if __name__ == '__main__':
     main()
